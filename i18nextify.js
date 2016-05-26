d1bd5b120004ecff37be313a93a4f78faa71d9b0
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-05-26 20:12:07 +0200

    rebuild

--- i18nextify.js
+++ i18nextify.js
@@ -5255,6 +5255,8 @@
 	    options.defaultNS = ns;
 	  }
 
+	  if (!options.ns.length) options.ns = ['translation'];
+
 	  // delay init from domReady
 	  if (!options.ele) {
 	    delete options.ele;
